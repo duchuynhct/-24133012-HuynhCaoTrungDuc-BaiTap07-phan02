32af14a465852789fd0d6e40ea5602ec44a0727c
Huynh Cao Trung Duc <huynhcaotrungduc@gmail.com>
2026-09-19 22:03:17 +0700

    chore: setup Spring Boot 3 project structure and database configuration

---
+++ src/main/java/vn/iotstar/SpringbootRestApiAjaxApplication.java
@@ -0,0 +1,12 @@
+package vn.iotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiAjaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestApiAjaxApplication.class, args);
+    }
+}
